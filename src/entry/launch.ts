a9d5dc2607762c546c17b9eff5211393b42ce10c
cedataproductteam <102552383+cedataproductteam@users.noreply.github.com>
2022-08-08 20:09:33 -0500

    Update launch.ts

--- src/entry/launch.ts
+++ src/entry/launch.ts
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import FHIR from "fhirclient";
 
-const clientId: string = process.env.REACT_APP_FHIR_CLIENT_ID || 'a9f3d781-004b-4e1a-9c1a-55875723438b';
+const clientId: string = process.env.REACT_APP_FHIR_CLIENT_ID || '2c9645fc-b344-4649-a00d-0d6393856ede';
 const scopes: string = 'patient/Condition.read patient/Patient.read patient/Observation.read patient/MedicationRequest.read launch online_access openid profile';
 const redirectUri: string = './';
 
